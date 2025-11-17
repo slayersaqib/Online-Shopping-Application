e397b2276689c3deafad8c15a951697457cddc23
Mohd Saqib <saqibsaifi2012ss@gmail.com>
2025-11-17 11:16:25 +0530

    Creating the Discovery Server

---
+++ discovery-server/src/main/java/com/msaqib/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.msaqib.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${eureka.username}")
+    private String username;
+    @Value("${eureka.password}")
+    private String password;
+
+    // Code is missing refer the video
+}
